6d2743a22acb54714edc3afcc0265e4e4bc96f3c
jj <jjhe9999@berkeley.edu>
2025-04-14 23:03:00 -0700

    cmake added

---
+++ build/CMakeFiles/final_project.dir/file1.cpp.o.d
@@ -0,0 +1,2 @@
+CMakeFiles/final_project.dir/file1.cpp.o: \
+  /Users/jj/Desktop/classes/184/cs184-final-project/file1.cpp
---
+++ build/CMakeFiles/my_project.dir/main.cpp.o.d
@@ -0,0 +1,2 @@
+CMakeFiles/my_project.dir/main.cpp.o: \
+  /Users/jj/Desktop/classes/184/cs184-final-project/main.cpp
