f42bd9a0382d0d8253e1f0350c1a5b29fe56b444
Rachel Hu <rachelhu@berkeley.edu>
2025-04-29 15:52:33 -0700

    untested bone code

--- src/animator.cpp
+++ src/animator.cpp
@@ -1,4 +1,8 @@
-// #include "animator.h"
+#include "animator.h"
+#include "Eigen/src/Geometry/Quaternion.h"
+#include "nanogui/common.h"
+#include <iostream>
+#include <vector>
 // #include <glad/glad.h>
 // #include "CGL/CGL.h"
 // #include <CGL/vector3D.h>
@@ -9,9 +13,129 @@
 // #include "utils.h"
 
 
-// using namespace CGL;
-// using namespace std;
+using namespace std;
 
+void Bone::interpolateAt(double time, Matrix4f parentTransform) {
+    Matrix4f localTransform = buildLocalTransform(time);
+    Matrix4f globalTransform = localTransform * parentTransform;    // try parent * local if this looks wonky
+    this->globalTransformation = globalTransform;
+    
+    for (auto child : *children) {
+        child->interpolateAt(time, globalTransform);
+    }
+
+}
+
+Vector3f lerp(Vector3f p0, Vector3f p1, double t) {
+    return (1 - t) * p0 + (t * p1);
+}
+
+Vector3f Bone::interpolatePosition(double time) const {
+    int i = findPositionIndex(time);
+
+    double t0 = positionTimes[i];
+    double t1 = positionTimes[i + 1];
+    Vector3f p0 = positionKeys[i];
+    Vector3f p1 = positionKeys[i + 1];
+
+    double t = (time - t0) / (t1 - t0);
+    Vector3f position = lerp(p0, p1, t);
+    return position;
+}
+
+Eigen::Quaternionf Bone::interpolateRotation(double time) const {
+    int i = findRotationIndex(time);
+    
+    double t0 = rotationTimes[i];
+    double t1 = rotationTimes[i + 1];
+    Eigen::Quaternionf q0 = rotationKeys[i];
+    Eigen::Quaternionf q1 = rotationKeys[i + 1];
+
+    double t = (time - t0) / (t1 - t0);
+    Eigen::Quaternionf rotation = q0.slerp(t, q1);
+    return rotation;
+}
+
+Vector3f Bone::interpolateScaling(double time) const {
+    int i = findScalingIndex(time);
+    
+    double t0 = scalingTimes[i];
+    double t1 = scalingTimes[i + 1];
+    Vector3f p0 = scalingKeys[i];
+    Vector3f p1 = scalingKeys[i + 1];
+
+    double t = (time - t0) / (t1 - t0);
+    Vector3f scale = lerp(p0, p1, t);
+    return scale;
+}
+
+int Bone::findPositionIndex(double time) const {
+    if (positionTimes.empty()) {
+        std::cout << "Error: Position times empty." << std::endl;
+        return -1;
+    }
+    if (time <= positionTimes[0]) {
+        return 0;
+    }
+    if (time >= positionTimes[-1]){
+        return positionTimes.size() - 2;
+    }
+    for (int i = 0; i < positionTimes.size() - 1; i++) {
+        if (time >= positionTimes[i] && time < positionTimes[i + 1]) {
+            return i;
+        }   
+    }
+    std::cout << "Time stamp out of range for positions." << std::endl;
+    return -1;
+}
+
+int Bone::findRotationIndex(double time) const {
+    if (rotationTimes.empty()) {
+        std::cout << "Error: Position times empty." << std::endl;
+        return -1;
+    }
+    if (time <= rotationTimes[0]) {
+        return 0;
+    }
+    if (time >= rotationTimes[-1]){
+        return rotationTimes.size() - 2;
+    }
+    for (int i = 0; i < rotationTimes.size() - 1; i++) {
+        if (time >= rotationTimes[i] && time < rotationTimes[i + 1]) {
+            return i;
+        }
+    }
+    std::cout << "Time stamp out of range for rotations." << std::endl;
+    return -1;
+}
+
+int Bone::findScalingIndex(double time) const {
+    if (scalingTimes.empty()) {
+        std::cout << "Error: Position times empty." << std::endl;
+        return -1;
+    }
+    if (time <= scalingTimes[0]) {
+        return 0;
+    }
+    if (time >= scalingTimes[-1]){
+        return scalingTimes.size() - 2;
+    }
+    for (int i = 0; i < scalingTimes.size() - 1; i++) {
+        if (time >= scalingTimes[i] && time < scalingTimes[i + 1]) {
+            return i;
+        }
+    }
+    std::cout << "Time stamp out of range for scaling." << std::endl;
+    return -1;
+}
+
+Matrix4f Bone::buildLocalTransform(double time) {     
+    Eigen::Vector3f posTransform = interpolatePosition(time);
+    Eigen::Quaternionf rotateTransform = interpolateRotation(time);
+    Eigen::Vector3f scaleTransform = interpolateScaling(time);
+
+    return (Eigen::Translation3f(posTransform) * rotateTransform * Eigen::Scaling(scaleTransform)).matrix();
+}
 // KeyFrameAnimator::KeyFrameAnimator(std::string project_root, Screen *screen)
 // : m_project_root(project_root) {
 //   this->screen = screen;
--- src/animator.h
+++ src/animator.h
@@ -1,6 +1,8 @@
 #ifndef ANIMATOR_H
 #define ANIMATOR_H
 
+// #include "Eigen/src/Geometry/Quaternion.h"
+#include "nanogui/common.h"
 #include <glad/glad.h>
 #include <nanogui/screen.h>
 #include <Eigen/Core>
@@ -11,49 +13,92 @@ using namespace nanogui;
 using namespace std;
 
 
-class Vertex {
-public:
+struct Vertex {
     Vector3f position;
     Vector3f normal;
     Vector3f color;
-    std::vector<GLuint> boneIndices;
-    std::vector<float> weights;
+    vector<GLuint> boneIndices;
+    vector<float> weights;
     
 };
 
 
-class Bone {
-public:
-    string name;
-    Matrix4f offsetMatrix;
-    std::vector<Bone*>* children;
-    Matrix4f transformation;
+struct Bone {
+    Bone() {};
+    ~Bone();
 
-    vector<double> positionTimes;
+// #################### ATTRIBUTES ####################
+    // identifier for this bone (aiBone.mName)
+    string name;
+    // transforms from mesh space to bone space in bind pose (aiBone.mOffsetMatrix)
+    Matrix4f offsetMatrix;
+    // list of points to all children of this bone in the overall bone hierarchy (aiNode.mChildren)
+    vector<Bone*>* children;
+    // transformation relative to the node's parent at rest (aiNode.mTransformation)
+    Matrix4f restLocalTransformation;
+    // Vector3f restPosition;
+    // Eigen::Quaternionf restRotation;
+    // Vector3f restScaling;
+
+    // transformationn relative to the node's parent during the animation (interpolated)
+    // Matrix4f localTransformation;
+    // total transformation of this bone at some time during the animation (interpolated, local * parent)
+    Matrix4f globalTransformation;
+
+    // times corresponding to each position key (aiNodeAnim.mPositionKeys[i].mTime)
+    std::vector<double> positionTimes;
+    // position keys of this animation channel (aiNodeAnim.mPositionKeys)
     vector<Eigen::Vector3f> positionKeys;
 
+    // times corresponding to each rotation key (aiNodeAnim.mRotationKeys[i].mTime)
     vector<double> rotationTimes;
+    // rotation keys of this animation channel (aiNodeAnim.mRotationKeys)
     vector<Eigen::Quaternionf> rotationKeys;
     
+    // times corresponding to each scaling key (aiNodeAnim.mScalingKeys[i].mTime)
     vector<double> scalingTimes;
+    // scaling keys of this animation channel (aiNodeAnim.mScalingKeys)
     vector<Eigen::Vector3f> scalingKeys;
-    // std::vector<Vertex> vertices;
+    // vector<Vertex> vertices;
+
+// #################### FUNCTIONS ####################
+    void interpolateAt(double time, Matrix4f parentTransform);
+
+private:
+    Vector3f interpolatePosition(double time) const;
+    Eigen::Quaternionf interpolateRotation(double time) const;
+    Vector3f interpolateScaling(double time) const;
+
+    int findPositionIndex(double time) const;
+    int findRotationIndex(double time) const;
+    int findScalingIndex(double time) const;
+
+    Matrix4f buildLocalTransform(double time);
 };
 
 
-class Mesh {
+struct Mesh {
+    // variables for rendering purposes
     GLuint VAO=0;
     GLuint indexCount;
-    std::unordered_map<string, Bone*>* bones;
+    // mesh architecture
+    unordered_map<string, Bone*>* bones;
+    Bone* rootBone;
     vector<Vertex>* vertices;
+
+    void animateAt(double time);
 };
 
 
-class Animation {
-public:
+struct Animation {
     Mesh* character; // full object
     double duration;
-    
+
+    double startTime;
+    double endTime;
+    double timestep;
+
+    void animate();
 };
 
 
